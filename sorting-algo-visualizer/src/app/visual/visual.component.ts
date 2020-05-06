3b758e9133d0fe72a6cd0b4bb5d6cdcde03303a1
dmensah1 <dmensah4@uwo.ca>
2020-05-06 15:22:29 -0400

    Bubble Sort implemented

--- sorting-algo-visualizer/src/app/visual/visual.component.ts
+++ sorting-algo-visualizer/src/app/visual/visual.component.ts
@@ -30,33 +30,52 @@ export class BodyComponent implements OnInit {
     return Math.floor(Math.random() * (250 - 50 + 1)) + 50;
   }
 
+  // setters to choose the type of sorting
   setQuick() {
     this.doQuick = true;
   }
-
   setBubble() {
     this.doBubble = true;
   }
-
   setMerge() {
     this.doMerge = true;
   }
 
   onChange(val) {
+    // clearing array
+    this.visualArr.splice(0, this.sliderVal);
+
     this.sliderVal = val;
+
+    // populating the array according to new size picked by user
+    for (let i = 0; i < this.sliderVal; i++) {
+      this.visualArr.push(this.createRandomNum());
+    }
+
   }
 
   performSort() {
     if (this.doQuick) {
-      // perform quick sort here
+      this.doQuick = false;
+
+
     } else if (this.doMerge) {
 
-      // perform merge sort here
+      this.doMerge = false;
+
 
     } else if (this.doBubble) {
+      this.doBubble = false;
 
-      // perform bubble sort here
-
+      for (let i = 0; i < this.sliderVal; i++) {
+        for (let j = 0; j < this.sliderVal; j++) {
+          if (this.visualArr[j] > this.visualArr[j + 1]) {
+            const temp = this.visualArr[j];
+            this.visualArr[j] = this.visualArr[j + 1];
+            this.visualArr[j + 1] = temp;
+          }
+        }
+      }
     }
   }
 }
