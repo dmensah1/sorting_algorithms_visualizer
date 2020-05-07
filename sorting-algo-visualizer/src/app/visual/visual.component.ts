8ab694f814d5e6fc2b75d85430ffec0c0779ee7f
dmensah1 <dmensah4@uwo.ca>
2020-05-07 15:03:05 -0400

    Bubble sort visuals fully working

--- sorting-algo-visualizer/src/app/visual/visual.component.ts
+++ sorting-algo-visualizer/src/app/visual/visual.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { timer } from 'rxjs';
+import { takeWhile } from 'rxjs/operators';
 
 @Component({
   selector: 'app-body',
@@ -13,6 +15,11 @@ export class BodyComponent implements OnInit {
   doMerge = false;
   doQuick = false;
   sliderVal = 30;
+  delayFlag = false;
+  currTime = Date.now();
+
+
+
 
   ngOnInit() {
 
@@ -51,31 +58,82 @@ export class BodyComponent implements OnInit {
     for (let i = 0; i < this.sliderVal; i++) {
       this.visualArr.push(this.createRandomNum());
     }
-
   }
 
   performSort() {
     if (this.doQuick) {
+
+
+
       this.doQuick = false;
-
-
     } else if (this.doMerge) {
 
+
+
       this.doMerge = false;
-
-
     } else if (this.doBubble) {
+      this.performBubble();
       this.doBubble = false;
+    }
+  }
 
-      for (let i = 0; i < this.sliderVal; i++) {
-        for (let j = 0; j < this.sliderVal; j++) {
+  performBubble() {
+    let i = 0;
+    let j = 0;
+
+    timer(0, 100)
+    .pipe(takeWhile(() => i < this.sliderVal))
+    .subscribe(() => {
+
+      while ( i < this.sliderVal) {
+        let change = false;
+
+        while ( j < this.sliderVal) {
+
           if (this.visualArr[j] > this.visualArr[j + 1]) {
+            change = true;
             const temp = this.visualArr[j];
             this.visualArr[j] = this.visualArr[j + 1];
             this.visualArr[j + 1] = temp;
+            break;
           }
+
+          j++;
+        }
+
+        if (change) { break; }
+        if (j === this.sliderVal) {
+          j = 0;
+          i++;
         }
       }
-    }
+    });
+
+
+    /* for (let i = 0; i < this.sliderVal; i++) {
+      for (let j = 0; j < this.sliderVal; j++) {
+        if (this.visualArr[j] > this.visualArr[j + 1]) {
+          const temp = this.visualArr[j];
+          this.visualArr[j] = this.visualArr[j + 1];
+          this.visualArr[j + 1] = temp;
+        }
+      }
+    }  */
+
   }
+
+  delayFunc() {
+    this.delayFlag = false;
+
+    while (!this.delayFlag) {
+      if ((Date.now() - this.currTime) > 1000) {
+        this.delayFlag = true;
+        console.log('delay occurred');
+        break;
+      }
+    }
+
+  }
+
+
 }
